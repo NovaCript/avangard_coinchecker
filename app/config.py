cc0a6524530a9aee5c9001de1ae1ecc9b494d34e
frutez <glebka.yatsenko@gmail.com>
2024-08-16 20:57:40 +0300

    create mono file

--- app/config.py
+++ app/config.py
@@ -1,6 +1,21 @@
-from dotenv import dotenv_values
+import os
+from dotenv import load_dotenv, dotenv_values
 
-config = dotenv_values(".env")
+dotenv_path = os.path.join(os.path.dirname(__file__), '..', '.env')
+load_dotenv(dotenv_path)
 
-API_KEY = config["API_KEY"]
-COINMARKETCAP_API_URL = config["COINMARKETCAP_API_URL"]
+config = dotenv_values(dotenv_path)
+
+class Config:
+    def __init__(self):
+        self.COINMARKETCAP_API_URL = os.getenv('COINMARKETCAP_API_URL')
+        self.API_KEY = os.getenv('API_KEY')
+        self.TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+        if not self.TELEGRAM_TOKEN:
+            raise ValueError('TELEGRAM_TOKEN не задан в файле .env')
+        if not self.API_KEY:
+            raise ValueError('API_KEY не задан в файле .env')
+        if not self.COINMARKETCAP_API_URL:
+            raise ValueError('COINMARKETCAP_API_URL не задан в файле .env')
+
+settings = Config()
--- app/main.py
+++ app/main.py
@@ -1,22 +1,91 @@
-import time
-from coinmarket.coinmarketcap_api import get_latest_price
-from coinmarket.models import CryptoCurrencyTracker
+from aiogram import Router, Bot, Dispatcher
+from aiogram.filters import CommandStart, Command
+from aiogram.types import Message
+import asyncio
+import requests
+from app.config import settings
 
 
-def main():
-    tracker = CryptoCurrencyTracker()
-    tracker.add_currency("LTC", 60, 50)
+API_TOKEN = settings.TELEGRAM_TOKEN
+bot = Bot(token=API_TOKEN)
+router = Router()
 
+user_currencies = {}
+chat_id = None
+
+def get_latest_price(symbol):
+    params = {"symbol": symbol, "convert": "USD"}
+    headers = {"X-CMC_PRO_API_KEY": settings.API_KEY}
+    response = requests.get(settings.COINMARKETCAP_API_URL, params=params, headers=headers)
+    data = response.json()
+    try:
+        price = data["data"][symbol]["quote"]["USD"]["price"]
+    except KeyError as e:
+        print(f"Error: {e}")
+        print(f"Response data: {data}")
+        return None
+    return price
+
+@router.message(CommandStart())
+async def command_start_handler(message: Message) -> None:
+    global chat_id
+    chat_id = message.chat.id
+    await message.answer(
+        "Приветствую!"
+        "\nЯ бот для отслеживания курса выбранной криптовалюты."
+        "\nВозможность отслеживания нескольких криптовалют одновременно,"
+        " хранение минимум двух значений для каждой валюты максимальный"
+        " и минимальный порог"
+        "\nФормат сообщения для отслеживания курса: "
+        "\n/add BTC 60 50",
+    )
+
+@router.message(Command("add"))
+async def add_currency_handler(message: Message) -> None:
+    global chat_id
+    try:
+        symbol, max_threshold, min_threshold = message.text.split()[1:]
+        max_threshold = float(max_threshold)
+        min_threshold = float(min_threshold)
+        if chat_id not in user_currencies:
+            user_currencies[chat_id] = {}
+        user_currencies[chat_id][symbol] = {"max_threshold": max_threshold, "min_threshold": min_threshold}
+        await message.answer(f"Добавлена криптовалюта {symbol} с порогами {max_threshold} и {min_threshold}")
+    except ValueError:
+        await message.answer("Неправильный формат сообщения. Используйте формат: BTC 60 50")
+
+async def check_currencies_periodically():
     while True:
-        for symbol in tracker.currencies.keys():
+        global chat_id
+        global user_currencies
+        if chat_id is None or not user_currencies:
+            await asyncio.sleep(1)
+            continue
+        prices = {}
+        for symbol in user_currencies[chat_id].keys():
             price = get_latest_price(symbol)
             if price is not None:
-                tracker.currencies[symbol].update_price(price)
-        notifications = tracker.check_thresholds()
-        for notification in notifications:
-            print(notification)
-        time.sleep(60)  # update prices every 1 minute
+                prices[symbol] = price
+        # проверяем пороги
+        notifications = []
+        for symbol, thresholds in user_currencies[chat_id].items():
+            if prices[symbol] >= thresholds["max_threshold"]:
+                notifications.append(f"{symbol} выше максимального порога: {thresholds['max_threshold']}")
+            elif prices[symbol] <= thresholds["min_threshold"]:
+                notifications.append(f"{symbol} ниже минимального порога: {thresholds['min_threshold']}")
+        if notifications:
+            await bot.send_message(chat_id, "\n".join(notifications))
+        await asyncio.sleep(60)
 
+async def main():
+    dp = Dispatcher()
+    dp.include_router(router)
 
-if __name__ == "__main__":
-    main()
+    asyncio.create_task(check_currencies_periodically())
+    await dp.start_polling(bot)
+
+if __name__ == '__main__':
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print('Exit')
